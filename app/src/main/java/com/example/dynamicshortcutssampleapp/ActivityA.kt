6a05b816b3a6dac61572fc89c543bbf4f5ed203b
atulKaushik19 <atul.p.kaushik@gmail.com>
2023-07-16 15:01:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dynamicshortcutssampleapp/ActivityA.kt
@@ -0,0 +1,11 @@
+package com.example.dynamicshortcutssampleapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/dynamicshortcutssampleapp/ActivityB.kt
@@ -0,0 +1,11 @@
+package com.example.dynamicshortcutssampleapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b_layout)
+    }
+}
